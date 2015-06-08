193a246b3872d20c6ed481e2ef8f434fccea741b
Phil Howell <phil@quae.co.uk>
2015-06-08 14:14:39 +0100

    Intial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:0.10.38-slim
+MAINTAINER Phil Howell <phil@quae.co.uk>
+
+ADD v0.7.2.tar.gz /tmp
+RUN mv /tmp/statsd-0.7.2 /statsd
+RUN cd /statsd && npm install statsd-influxdb-backend
+
+ADD config.js /statsd/config.js
+
+EXPOSE 8125/udp 8126/tcp
+
+CMD ["/usr/local/bin/node", "/statsd/stats.js", "/statsd/config.js"]
---
+++ config.js
@@ -0,0 +1,24 @@
+(function() {
+
+    return {
+        influxdb: {
+            host: process.env.INFLUXDB_HOST || "localhost",
+            port: process.env.INFLUXDB_PORT || 8086,
+            database: process.env.INFLUXDB_DATABASE || "statsd",
+            username: process.env.INFLUXDB_USERNAME || "root",
+            password: process.env.INFLUXDB_PASSWORD || "root",
+            flush: {
+                enable: true
+            },
+            proxy: {
+                enable: false,
+                suffix: 'raw',
+                flushInterval: 1000
+            }
+        },
+        port: 8125,
+        backends: ['./backends/console', 'statsd-influxdb-backend'],
+        debug: process.env.STATSD_DEBUG || false,
+        legacyNamespace: false
+    };
+})();
